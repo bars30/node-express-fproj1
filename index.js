df71c6018aff45dcde62bf796198e9552e61c361
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-10-05 17:31:59 +0400

    commented listen

--- index.js
+++ index.js
@@ -228,9 +228,9 @@ function verifyToken(req, res, next) {
 }
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 
-// module.exports = app;
+module.exports = app;
